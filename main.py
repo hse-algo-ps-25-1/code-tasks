f86b49bfdd1d96dc7c735151f7c1dabf6923b638
Егор Полешко <endlesssunsets@yandex.ru>
2026-09-23 17:49:21 +0300

    Загрузка решения задания 1, вариант 1
    Co-authored-by: Ульяна Аникиенко <274785558+ulanaanikienko@users.noreply.github.com>
    Co-authored-by: Руслана Шагеева <230561234+ruslana090574-collab@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-from profilehooks import profile
-
-
-def fibonacci_rec(n: int) -> int:
+def fibonacci_rec(n):
     """Возвращает N-е число Фибоначчи. Реализована рекурсивно согласно
     формуле вычисления последовательности.
 
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
 
 
-def fibonacci_iter(n: int) -> int:
+def fibonacci_iter(n):
     """Возвращает N-е число Фибоначчи. Реализована итеративно с использованием
     массива для хранения вычисляемых данных.
 
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n == 0:
+        return 0
+
+    numbers = [0, 1]
+
+    for i in range(2, n + 1):
+        numbers.append(numbers[i - 1] + numbers[i - 2])
+
+    return numbers[n]
 
 
-def fibonacci(n: int) -> int:
+def fibonacci(n):
     """Возвращает N-е число Фибоначчи. Реализована итеративно без использования массива.
 
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n == 0:
+        return 0
 
+    previous = 0
+    current = 1
+
+    for _ in range(2, n + 1):
+        previous, current = current, previous + current
+
+    return current
 
 def main():
     n = 35
